0f4db3a1cc3a55d482f9585d74f0667af8bb91c0
alexveryk <veryk.av@gmail.com>
2023-03-11 23:38:26 +0200

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Overlay, ModalWindow } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { url } = this.props;
+
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalWindow>
+          <img src={url} alt=" " />
+        </ModalWindow>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
+
+Modal.propTypes = {
+  url: PropTypes.string.isRequired,
+};
