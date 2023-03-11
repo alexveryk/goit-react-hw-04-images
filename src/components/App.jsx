4dbdabf9835e3d9c0210cba6c30cf6c309efc58e
alexveryk <veryk.av@gmail.com>
2023-03-11 23:38:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,92 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ProgressBar } from 'react-loader-spinner';
+import { getImages } from '../API/api';
+import { ContainerApp } from './App.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+import { toast, ToastContainer } from 'react-toastify';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+
+export class App extends Component {
+  state = {
+    images: [],
+    page: 1,
+    loading: false,
+    totalHits: 0,
+    imageName: '',
+    error: '',
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.page !== this.state.page ||
+      prevState.imageName !== this.state.imageName
+    ) {
+      this.setState({ isLoading: true });
+      try {
+        const { imageName, page } = this.state;
+
+        const fetchData = await getImages(imageName, page);
+        if (!fetchData.total) {
+          toast.error('No images found !', {
+            position: toast.POSITION.TOP_LEFT,
+          });
+        }
+
+        this.setState(({ images }) => ({
+          images: [...images, ...fetchData.hits],
+          totalHits: fetchData.total,
+        }));
+      } catch (error) {
+        this.setState({ error });
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
+  handleFormSubmit = value => {
+    this.setState({
+      imageName: value,
+      images: [],
+      page: 1,
+    });
+  };
+
+  handlePageChange = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+
+  render() {
+    const { isLoading, images, totalHits } = this.state;
+
+    return (
+      <ContainerApp>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {isLoading && (
+          <ProgressBar
+            height="80"
+            width="80"
+            ariaLabel="progress-bar-loading"
+            wrapperStyle={{
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            wrapperClass="progress-bar-wrapper"
+            borderColor="#fff"
+            barColor="#3f51b5"
+          />
+        )}
+        <ImageGallery imageName={images} />
+
+        {images.length < totalHits && (
+          <Button onChange={this.handlePageChange} />
+        )}
+
+        <ToastContainer autoClose={2000} />
+      </ContainerApp>
+    );
+  }
+}
