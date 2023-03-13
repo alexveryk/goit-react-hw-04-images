cb30f7ea132d6cfe2c730609fd64ac3d8c310afd
alexveryk <veryk.av@gmail.com>
2023-03-13 13:23:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ProgressBar } from 'react-loader-spinner';
 import { getImages } from '../API/api';
 import { ContainerApp } from './App.styled';
@@ -7,86 +7,69 @@ import { toast, ToastContainer } from 'react-toastify';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 
-export class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    loading: false,
-    totalHits: 0,
-    imageName: '',
-    error: '',
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
+  const [imageName, setImageName] = useState('');
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.imageName !== this.state.imageName
-    ) {
-      this.setState({ isLoading: true });
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const { imageName, page } = this.state;
-
-        const fetchData = await getImages(imageName, page);
-        if (!fetchData.total) {
+        setLoading(true);
+        const response = await getImages(imageName, page);
+        if (!response.total) {
           toast.error('No images found !', {
             position: toast.POSITION.TOP_LEFT,
           });
         }
-
-        this.setState(({ images }) => ({
-          images: [...images, ...fetchData.hits],
-          totalHits: fetchData.total,
-        }));
+        setImages(prevState => [...prevState, ...response.hits]);
+        setTotalHits(response.total);
       } catch (error) {
-        this.setState({ error });
+        console.log(error);
       } finally {
-        this.setState({ isLoading: false });
+        setLoading(false);
       }
+    };
+
+    if (imageName) {
+      fetchData();
     }
-  }
+  }, [imageName, page]);
 
-  handleFormSubmit = value => {
-    this.setState({
-      imageName: value,
-      images: [],
-      page: 1,
-    });
+  const handleFormSubmit = value => {
+    setImageName(value);
+    setImages([]);
+    setPage(1);
   };
 
-  handlePageChange = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const handlePageChange = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { isLoading, images, totalHits } = this.state;
+  return (
+    <ContainerApp>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {loading && (
+        <ProgressBar
+          height="80"
+          width="80"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+          wrapperClass="progress-bar-wrapper"
+          borderColor="#fff"
+          barColor="#3f51b5"
+        />
+      )}
+      <ImageGallery images={images} />
 
-    return (
-      <ContainerApp>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {isLoading && (
-          <ProgressBar
-            height="80"
-            width="80"
-            ariaLabel="progress-bar-loading"
-            wrapperStyle={{
-              marginLeft: 'auto',
-              marginRight: 'auto',
-            }}
-            wrapperClass="progress-bar-wrapper"
-            borderColor="#fff"
-            barColor="#3f51b5"
-          />
-        )}
-        <ImageGallery imageName={images} />
+      {images.length < totalHits && <Button onChange={handlePageChange} />}
 
-        {images.length < totalHits && (
-          <Button onChange={this.handlePageChange} />
-        )}
-
-        <ToastContainer autoClose={2000} />
-      </ContainerApp>
-    );
-  }
-}
+      <ToastContainer autoClose={2000} />
+    </ContainerApp>
+  );
+};
